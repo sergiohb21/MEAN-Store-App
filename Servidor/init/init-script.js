e2ee2b24f29f1d66067c32670895a2c870d32e32
Sergio Hernandez Home <sergiohb21@example.com>
2024-02-28 20:04:22 +0100

    init modificado

--- Servidor/init/init-script.js
+++ Servidor/init/init-script.js
@@ -7,7 +7,7 @@ db.categorias.insert([{"id":1,"name":"Clothes","image":"https://i.imgur.com/QkIa
 db.productos.insert([
     {
       "title": "Camiseta deportiva",
-      "price": 1999,
+      "price": 13,
       "description": "Camiseta deportiva transpirable",
       "image": "./assets/imagenes/camiseta_deportiva.webp",
       "category": 1,
@@ -16,7 +16,7 @@ db.productos.insert([
     },
     {
       "title": "Pantalones cortos",
-      "price": 2499,
+      "price": 24,
       "description": "Pantalones cortos cómodos para hacer ejercicio",
       "image": "./assets/imagenes/camiseta_deportiva.webp",
       "category": 1,
@@ -25,7 +25,7 @@ db.productos.insert([
     },
     {
       "title": "Sudadera con capucha",
-      "price": 3499,
+      "price": 34,
       "description": "Sudadera con capucha y bolsillo canguro",
       "image": "./assets/imagenes/sudadera_capucha.webp",
       "category": 1,
@@ -34,7 +34,7 @@ db.productos.insert([
     },
     {
       "title": "Calcetines deportivos",
-      "price": 999,
+      "price": 3,
       "description": "Calcetines deportivos cómodos y duraderos",
       "image": "./assets/imagenes/calcetines_deportivos.webp",
       "category": 1,
@@ -43,7 +43,7 @@ db.productos.insert([
     },
     {
       "title": "Teléfono inteligente",
-      "price": 6999,
+      "price": 600,
       "description": "Teléfono inteligente con pantalla táctil y cámara de alta resolución",
       "image": "./assets/imagenes/telefono_inteligente.webp",
       "category": 2,
@@ -52,7 +52,7 @@ db.productos.insert([
     },
     {
       "title": "Laptop",
-      "price": 12999,
+      "price": 900,
       "description": "Laptop ultradelgada con procesador potente y larga duración de la batería",
       "image": "./assets/imagenes/laptop.webp",
       "category": 2,
@@ -61,7 +61,7 @@ db.productos.insert([
     },
     {
       "title": "Auriculares inalámbricos",
-      "price": 2999,
+      "price": 29,
       "description": "Auriculares inalámbricos con cancelación de ruido activa",
       "image": "./assets/imagenes/auriculares_inalambricos.webp",
       "category": 2,
@@ -70,7 +70,7 @@ db.productos.insert([
     },
     {
       "title": "Smartwatch",
-      "price": 4999,
+      "price": 49,
       "description": "Smartwatch con monitor de frecuencia cardíaca y seguimiento de actividad",
       "image": "./assets/imagenes/smartwatch.webp",
       "category": 2,
@@ -79,7 +79,7 @@ db.productos.insert([
     },
     {
       "title": "Silla de oficina ergonómica",
-      "price": 4999,
+      "price": 49,
       "description": "Silla de oficina con respaldo ajustable y soporte lumbar",
       "image": "./assets/imagenes/silla_oficina.webp",
       "category": 3,
@@ -88,7 +88,7 @@ db.productos.insert([
     },
     {
       "title": "Escritorio de madera",
-      "price": 7999,
+      "price": 79,
       "description": "Escritorio de madera maciza con amplio espacio de trabajo",
       "image": "./assets/imagenes/escritorio_madera.webp",
       "category": 3,
@@ -97,7 +97,7 @@ db.productos.insert([
     },
     {
       "title": "Sofá de cuero",
-      "price": 14999,
+      "price": 1499,
       "description": "Sofá de cuero genuino con asientos reclinables",
       "image": "./assets/imagenes/sofa_cuero.webp",
       "category": 3,
@@ -106,7 +106,7 @@ db.productos.insert([
     },
     {
       "title": "Mesa de comedor",
-      "price": 9999,
+      "price": 250,
       "description": "Mesa de comedor con superficie de vidrio templado y patas de acero inoxidable",
       "image": "./assets/imagenes/mesa_comedor.webp",
       "category": 3,
@@ -115,7 +115,7 @@ db.productos.insert([
     },
     {
       "title": "Zapatillas deportivas",
-      "price": 3999,
+      "price": 62,
       "description": "Zapatillas deportivas con suela amortiguada y material transpirable",
       "image": "./assets/imagenes/zapatillas_deportivas.webp",
       "category": 4,
@@ -124,7 +124,7 @@ db.productos.insert([
     },
     {
       "title": "Botas de senderismo",
-      "price": 5999,
+      "price": 59,
       "description": "Botas de senderismo resistentes al agua y con suela antideslizante",
       "image": "./assets/imagenes/botas_senderismo.webp",
       "category": 4,
@@ -133,7 +133,7 @@ db.productos.insert([
     },
     {
       "title": "Zapatos de vestir",
-      "price": 4999,
+      "price": 49,
       "description": "Zapatos de vestir elegantes con diseño clásico",
       "image": "./assets/imagenes/zapatos_vestir.webp",
       "category": 4,
@@ -142,7 +142,7 @@ db.productos.insert([
     },
     {
       "title": "Sandalias de playa",
-      "price": 2999,
+      "price": 18,
       "description": "Sandalias de playa cómodas con correas ajustables",
       "image": "./assets/imagenes/sandalias_playa.webp",
       "category": 4,
@@ -151,7 +151,7 @@ db.productos.insert([
     },
     {
       "title": "Set de herramientas",
-      "price": 2999,
+      "price": 49,
       "description": "Set de herramientas completo con destornilladores, llaves y martillo",
       "image": "./assets/imagenes/set_herramientas.webp",
       "category": 5,
@@ -160,7 +160,7 @@ db.productos.insert([
     },
     {
       "title": "Lámpara de escritorio",
-      "price": 1999,
+      "price": 19,
       "description": "Lámpara de escritorio LED con brazo flexible",
       "image": "./assets/imagenes/lampara_escritorio.webp",
       "category": 5,
@@ -169,7 +169,7 @@ db.productos.insert([
     },
     {
       "title": "Caja de almacenamiento",
-      "price": 1499,
+      "price": 14,
       "description": "Caja de almacenamiento de plástico resistente con tapa hermética",
       "image": "./assets/imagenes/caja_almacenamiento.webp",
       "category": 5,
@@ -178,7 +178,7 @@ db.productos.insert([
     },
     {
       "title": "Reloj de pared",
-      "price": 999,
+      "price": 9,
       "description": "Reloj de pared decorativo con diseño moderno",
       "image": "./assets/imagenes/reloj_pared.webp",
       "category": 5,
